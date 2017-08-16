a1a44882540a7394e4e53beda0ddee5d68307b6f
Ava Xu (Beyondsoft Corporation) <v-xxia@microsoft.com>
2017-08-16 17:58:49 +0800

    addproject

---
+++ app/src/main/java/com/javaandroid/MainActivity.java
@@ -0,0 +1,298 @@
+package com.javaandroid;
+
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.VisibleForTesting;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.test.espresso.idling.CountingIdlingResource;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import com.microsoft.azure.mobile.MobileCenter;
+import com.microsoft.azure.mobile.analytics.Analytics;
+import com.microsoft.azure.mobile.crashes.Crashes;
+import com.microsoft.azure.mobile.crashes.CrashesListener;
+import com.microsoft.azure.mobile.crashes.ingestion.models.ErrorAttachmentLog;
+import com.microsoft.azure.mobile.crashes.model.ErrorReport;
+import com.microsoft.azure.mobile.push.Push;
+import com.microsoft.azure.mobile.utils.async.MobileCenterConsumer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = "MobileCenterSasquatch";
+    @VisibleForTesting
+    static final CountingIdlingResource crashesIdlingResource = new CountingIdlingResource("crashes");
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /* Set listeners. */
+        Push.setListener(new MyPushListener());
+
+        CrashesListener customListener = new CrashesListener() {
+            @Override
+            public boolean shouldProcess(ErrorReport report) {
+                return true;
+            }
+
+            @Override
+            public boolean shouldAwaitUserConfirmation() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder
+                        .setTitle(R.string.crash_confirmation_dialog_title)
+                        .setMessage(R.string.crash_confirmation_dialog_message)
+                        .setPositiveButton(R.string.crash_confirmation_dialog_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Crashes.notifyUserConfirmation(Crashes.SEND);
+                                Analytics.trackEvent("onClick");
+                            }
+                        })
+                        .setNegativeButton(R.string.crash_confirmation_dialog_not_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Crashes.notifyUserConfirmation(Crashes.DONT_SEND);
+                            }
+                        })
+                        .setNeutralButton(R.string.crash_confirmation_dialog_always_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Crashes.notifyUserConfirmation(Crashes.ALWAYS_SEND);
+                            }
+                        });
+                builder.create().show();
+                return true;
+            }
+
+            @Override
+            public Iterable<ErrorAttachmentLog> getErrorAttachments(ErrorReport report) {
+                /* Attach some text. */
+                ErrorAttachmentLog textLog = ErrorAttachmentLog.attachmentWithText("This is a text attachment.", "text.txt");
+
+                 /* Attach app icon. */
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                byte[] bitMapData = stream.toByteArray();
+                ErrorAttachmentLog binaryLog = ErrorAttachmentLog.attachmentWithBinary(bitMapData, "ic_launcher.jpeg", "image/jpeg");
+
+                /* Return attachments as list. */
+                return Arrays.asList(textLog, binaryLog);
+            }
+
+            @Override
+            public void onBeforeSending(ErrorReport report) {
+                Toast.makeText(MainActivity.this, R.string.crash_before_sending, Toast.LENGTH_SHORT).show();
+                crashesIdlingResource.increment();
+            }
+
+            @Override
+            public void onSendingFailed(ErrorReport report, Exception e) {
+                Toast.makeText(MainActivity.this, R.string.crash_sent_failed, Toast.LENGTH_SHORT).show();
+                crashesIdlingResource.decrement();
+            }
+
+            @Override
+            public void onSendingSucceeded(ErrorReport report) {
+                String message = String.format("%s\nCrash ID: %s", getString(R.string.crash_sent_succeeded), report.getId());
+                if (report.getThrowable() != null){
+                    message += String.format("\nThrowable: %s", report.getThrowable().toString());
+                }
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                crashesIdlingResource.decrement();
+            }
+            // Implement all callbacks here.
+        };
+        Crashes.setListener(customListener);
+            /* Print last crash. */
+        Crashes.hasCrashedInLastSession().thenAccept(new MobileCenterConsumer<Boolean>() {
+
+            @Override
+            public void accept(Boolean crashed) {
+                Log.i(LOG_TAG, "Crashes.hasCrashedInLastSession=" + crashed);
+            }
+        });
+        Crashes.getLastSessionCrashReport().thenAccept(new MobileCenterConsumer<ErrorReport>() {
+
+            @Override
+            public void accept(ErrorReport data) {
+                if (data != null) {
+                    Log.i(LOG_TAG, "Crashes.getLastSessionCrashReport().getThrowable()=", data.getThrowable());
+                }
+            }
+        });
+
+        Crashes.hasCrashedInLastSession();
+        Crashes.getLastSessionCrashReport();
+        // Depending on the user's choice, call Crashes.notifyUserConfirmation() with the right value.
+        Crashes.notifyUserConfirmation(Crashes.DONT_SEND);
+        Crashes.notifyUserConfirmation(Crashes.SEND);
+        Crashes.notifyUserConfirmation(Crashes.ALWAYS_SEND);
+        //Start Mobile center
+        MobileCenter.setLogUrl("https://in-staging-south-centralus.staging.avalanch.es");
+        MobileCenter.start(getApplication(), "e678a540-82f2-4c6a-9af5-491e883ba6e2", Analytics.class, Crashes.class,Push.class);
+
+        Log.e("installID", "" + MobileCenter.getInstallId().get());
+        /* Print last crash. */
+
+        Analytics.trackEvent("onCreateOptionsMenu");
+        Analytics.trackEvent("onOptionsItemSelected");
+        //nalytics.trackEvent("getPageTitle");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if(position==2){
+                Crashes.generateTestCrash();
+            }
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+    }
+}
