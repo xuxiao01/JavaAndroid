d68214b0aa0b73cac81f0056f85ea951e0506b4f
xuxiao01 <o-xuxiao01@beyondsoft.com>
2017-08-31 10:27:50 +0800

    add distribute

---
+++ app/src/main/java/com/javaandroid/MyDistributeListener.java
@@ -0,0 +1,63 @@
+package com.javaandroid;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
+
+import com.microsoft.azure.mobile.distribute.Distribute;
+import com.microsoft.azure.mobile.distribute.DistributeListener;
+import com.microsoft.azure.mobile.distribute.ReleaseDetails;
+import com.microsoft.azure.mobile.distribute.UpdateAction;
+
+/**
+ * Created by v-xxia on 8/31/2017.
+ */
+
+public class MyDistributeListener implements DistributeListener {
+
+	@Override
+	public boolean onReleaseAvailable(Activity activity, ReleaseDetails releaseDetails) {
+
+		// Look at releaseDetails public methods to get version information, release notes text or release notes URL
+		String versionName = releaseDetails.getShortVersion();
+		int versionCode = releaseDetails.getVersion();
+		String releaseNotes = releaseDetails.getReleaseNotes();
+		Uri releaseNotesUrl = releaseDetails.getReleaseNotesUrl();
+
+		// Build our own dialog title and message
+		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
+		dialogBuilder.setTitle("Version " + versionName + " available!"); // you should use a string resource instead of course, this is just to simplify example
+		dialogBuilder.setMessage(releaseNotes);
+
+		// Mimic default SDK buttons
+		dialogBuilder.setPositiveButton(com.microsoft.azure.mobile.distribute.R.string.mobile_center_distribute_update_dialog_download, new DialogInterface.OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				// This method is used to tell the SDK what button was clicked
+				Distribute.notifyUpdateAction(UpdateAction.UPDATE);
+			}
+		});
+
+		// We can postpone the release only if the update is not mandatory
+		if (!releaseDetails.isMandatoryUpdate()) {
+			dialogBuilder.setNegativeButton(com.microsoft.azure.mobile.distribute.R.string.mobile_center_distribute_update_dialog_postpone, new DialogInterface.OnClickListener() {
+
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+
+					// This method is used to tell the SDK what button was clicked
+					Distribute.notifyUpdateAction(UpdateAction.POSTPONE);
+				}
+			});
+		}
+		dialogBuilder.setCancelable(false); // if it's cancelable you should map cancel to postpone, but only for optional updates
+		dialogBuilder.create().show();
+
+		// Return true if you are using your own dialog, false otherwise
+		return true;
+	}
+
+}
